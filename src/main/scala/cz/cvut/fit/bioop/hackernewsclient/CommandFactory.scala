bd64a8d3503a72d17822a593cc6be45cb5b99f9b
svoboel5 <svoboel5@fit.cvut.cz>
2020-11-11 14:00:37 +0100

    Implementation of NotFoundCommand

--- src/main/scala/cz/cvut/fit/bioop/hackernewsclient/CommandFactory.scala
+++ src/main/scala/cz/cvut/fit/bioop/hackernewsclient/CommandFactory.scala
@@ -7,7 +7,7 @@ object CommandFactory {
     val commandOptions = args.slice(1, args.length)
     args(0) match {
       case "top-stories" => new TopStoriesCommand(appOptions, commandOptions)
-      case notFoundName => new NotFoundCommand(notFoundName + "-command-not-found", appOptions, commandOptions)
+      case notFoundName => new NotFoundCommand(notFoundName, appOptions, commandOptions)
     }
   }
 
--- src/main/scala/cz/cvut/fit/bioop/hackernewsclient/commands/NotFoundCommand.scala
+++ src/main/scala/cz/cvut/fit/bioop/hackernewsclient/commands/NotFoundCommand.scala
@@ -2,5 +2,8 @@ package cz.cvut.fit.bioop.hackernewsclient.commands
 import cz.cvut.fit.bioop.hackernewsclient.AppOptions
 
 class NotFoundCommand(val name: String, val appOptions: AppOptions, val commandOptions: Array[String]) extends Command {
-  override def execute(): Unit = ???
+  override def execute(): Unit = {
+    println("Command \"" + name + "\" not found")
+    println("Try \"hackernewsclient --help\" for possible options")
+  }
 }
