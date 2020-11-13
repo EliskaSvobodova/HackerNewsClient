eff1eeafdc65a132a94fde87f0da9f4074ac7be8
svoboel5 <svoboel5@fit.cvut.cz>
2020-11-13 14:17:15 +0100

    Pretty print of AppOptions

--- src/main/scala/cz/cvut/fit/bioop/hackernewsclient/AppOptions.scala
+++ src/main/scala/cz/cvut/fit/bioop/hackernewsclient/AppOptions.scala
@@ -1,11 +1,17 @@
 package cz.cvut.fit.bioop.hackernewsclient
 
-case class AppOptions(help: Boolean = false) {
-  override def toString: String = "AppOptions[help = " + help + "]"
+case class AppOptions(help: Boolean = false, limitOfStories: Int = 10) {
+  override def toString: String = {
+    val fields = for {
+      field <- getClass.getDeclaredFields
+    } yield field.getName + " = " + field.get(this)
+    fields.mkString("AppOptions[", ", ", "]")
+  }
 
   def processOptions() = {
     if(help){
       println("Very helpful help")
+      for(f <- getClass.getDeclaredFields)  yield f.toString
     }
   }
 }
