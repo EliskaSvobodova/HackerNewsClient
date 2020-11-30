c1659ee924500fdad09f1618c5788a6083ffe6f6
svoboel5 <svoboel5@fit.cvut.cz>
2020-11-30 11:38:59 +0100

    Test ApiClient

---
+++ src/test/scala/cz/cvut/fit/bioop/hackernewsclient/api/ApiClientTest.scala
@@ -0,0 +1,17 @@
+package cz.cvut.fit.bioop.hackernewsclient.api
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class ApiClientTest extends AnyFunSuite {
+
+  test("testGetUser non existent user") {
+    val user = ApiClient.getUser("hopefullyNoOne2498357")
+    assert(user.isEmpty)
+  }
+
+  test("testGetItem non existent item") {
+    val item = ApiClient.getItem(-1)
+    assert(item.isEmpty)
+  }
+
+}
